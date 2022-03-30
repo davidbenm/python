6d79ca2d78df342cdb103444acef73cf181fd475
davidbenm <dbenitezm92@gmail.com>
2022-03-30 18:19:28 -0300

    Create strings_methods.py

---
+++ python_basics/strings_methods.py
@@ -0,0 +1,16 @@
+phrase = " mOUnTains "
+print(phrase.upper())
+print(phrase.lower())
+print(phrase.strip())
+print(phrase.lstrip())
+print(phrase.rstrip())
+print(phrase.lower().count("u"))
+print(phrase.lower().endswith("ains "))
+print(phrase.isnumeric())
+print(" ".join(["This", "is", "a", "phrase", "joined", "by", "spaces"]))
+print("...".join(["This", "is", "a", "phrase", "joined", "by", "triple", "dots"]))
+print("This is an example of the split function".split())
+
+
+
+    
